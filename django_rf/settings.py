c1429043b23602ec48f15722c68903051cd1a95e
Ilia Grigorev <ilia_grigorev@mail.ru>
2023-05-04 21:36:28 +0200

    env

--- django_rf/settings.py
+++ django_rf/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,9 +79,9 @@ WSGI_APPLICATION = 'django_rf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_rest_framework',
-        'USER': 'postgres',
-        'PASSWORD': 'Stleitenant7789',
+        'NAME': os.getenv('name'),
+        'USER': os.getenv('user'),
+        'PASSWORD': os.getenv('password'),
         'HOST': 'localhost',
         'PORT': '5432',
 
